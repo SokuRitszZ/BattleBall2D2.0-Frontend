52f223b72efc011ec3ae55cf9b33a71e1dcb9da7
Andrew Leung <1062708758@qq.com>
2023-02-07 21:41:32 +0800

    feat: route + index view

--- src/pages/IndexView/index.tsx
+++ src/pages/IndexView/index.tsx
@@ -1,9 +1,23 @@
-function Index() {
+import { Link } from "react-router-dom";
+
+function IndexView() {
   return (
-    <>
-      <div>IndexView</div>
-    </>
+    <div className="w-screen h-screen min-w-[1200px] min-h-[700px] flex items-center justify-center">
+      <div>
+        <div className="text-center text-7xl">
+          <p> Battle Ball 2D </p>
+          <p className="text-5xl mt-4 text-blue-500"> ---Remaster Version </p>
+        </div>
+        <div className="w-full text-center mt-5">
+          <Link to="/account">
+            <button className="m-auto text-3xl px-3 py-2 bg-amber-600 rounded-xl mt-2 text-gray-100 transition hover:bg-amber-700 active:bg-amber-800">
+              START
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default Index;
+export default IndexView;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { RouteObject } from "react-router-dom";
+import AccountView from "../pages/AccountView";
 import IndexView from "../pages/IndexView";
 
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <IndexView/>
-  }
+    element: <IndexView />,
+  },
+  {
+    path: '/account',
+    element: <AccountView />
+  },
 ];
 
 export default routes;
