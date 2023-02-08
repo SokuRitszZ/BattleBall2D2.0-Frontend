8b0f319fbf2a69cc7b1c23840947655233cfe57c
Andrew Leung <1062708758@qq.com>
2023-02-08 17:17:52 +0800

    fix:component Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,9 @@
-function Container(props: {children: any}) {
+function Container(props: { children: any }) {
   return (
     <div className="w-screen h-screen min-w-[1200px] min-h-[800px] flex justify-center items-center">
       {props.children}
     </div>
-  )
+  );
 }
 
 export default Container;
